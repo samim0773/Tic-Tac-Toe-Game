309d360192f9af033df0dc3de23d421b09502945
Samim Aktar <samimaktar0773@gmail.com>
2021-10-11 11:59:51 +0530

    UC1 add comment and solve some error

--- src/com/bridgelab/tictactoe/TicTacToeGame.java
+++ src/com/bridgelab/tictactoe/TicTacToeGame.java
@@ -1,28 +1,30 @@
 package com.bridgelab.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	
 	//declare global array 
-	static char board [] = new char [10];	
+	static char board [] = new char [10];
 	
 	public static void main(String[] args) {
 		//print welcome message
-		System.out.println("Welcome To Tic Tac Toe Game.. ");
-		// calling method creat board
+		System.out.println("****** Welcome To Tic Tac Toe Game ****** ");
+		// calling method 
 		creatBoard();
 	}
 	
-	//********** UC1 - Create Board ***************
+	//********** UC1 - Create Board ***************//
 	
-	// Create method creat Board 
+	// Create method create Board 
 	public static void creatBoard() {
 		
 		//declare for loop for add value in array
-		for ( int i = 1; i<board.length; i++ ) {
+		for ( int i = 0; i<board.length; i++ ) {
 			
 			// Store empty char in array
 			board [i] =' ';	
-		}			
+		}
 	}
-
 }
+
