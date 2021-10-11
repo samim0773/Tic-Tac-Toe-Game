3bbd46aef5a2d73f9e34e1d73230769880a8de8e
Samim Aktar <samimaktar0773@gmail.com>
2021-10-11 09:03:09 +0530

    Done UC1 Create Board

--- src/com/bridgelab/tictactoe/TicTacToeGame.java
+++ src/com/bridgelab/tictactoe/TicTacToeGame.java
@@ -1,9 +1,28 @@
 package com.bridgelab.tictactoe;
 
 public class TicTacToeGame {
+	
+	//declare global array 
+	static char board [] = new char [10];	
+	
 	public static void main(String[] args) {
 		//print welcome message
-		System.out.println("Welcome To Tic Tac Toe Game.. ");	
+		System.out.println("Welcome To Tic Tac Toe Game.. ");
+		// calling method creat board
+		creatBoard();
+	}
+	
+	//********** UC1 - Create Board ***************
+	
+	// Create method creat Board 
+	public static void creatBoard() {
+		
+		//declare for loop for add value in array
+		for ( int i = 1; i<board.length; i++ ) {
+			
+			// Store empty char in array
+			board [i] =' ';	
+		}			
 	}
 
 }
